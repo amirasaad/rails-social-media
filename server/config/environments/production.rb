652734a7de9da643b4065946e63b27c119ed691f
Amir Asaad <asaad.amir@icloud.com>
2021-01-11 15:10:26 +0200

    Change auth from login to plain SMTP

--- server/config/environments/production.rb
+++ server/config/environments/production.rb
@@ -92,8 +92,9 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.action_mailer.default_url_options = { host: 'socialrate.herokuapp.com' }
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'socialrate.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
