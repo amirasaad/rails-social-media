e5a7bb08f62ae73f995db656755d126f5903ada3
Amir Asaad <asaad.amir@icloud.com>
2021-01-11 13:33:47 +0200

    Change SMTP auth from plain to login

--- server/config/environments/production.rb
+++ server/config/environments/production.rb
@@ -102,7 +102,7 @@ Rails.application.configure do
     address: ENV['SMTP_ADDRESS'],
     port: ENV['SMTP_PORT'],
     domain: ENV['SMTP_DOMAIN'],
-    authentication: 'plain',
+    authentication: :login,
     enable_starttls_auto: true,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD']
